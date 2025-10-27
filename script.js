8475e574236651b4306dd38030b0ac778f85f097
Dhairya <dhairyashah.ai@gmail.com>
2025-10-26 21:07:17 -0400

    Added table output and giphy

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('#enter-btn');
 const loc = document.querySelector('#loc');
 const tbody = document.querySelector('#tbody');
+const img = document.querySelector('img');
 
 btn.addEventListener('click', async () => {
     // clear the table
@@ -14,6 +15,7 @@ btn.addEventListener('click', async () => {
     try {
         const weather = await getWeather(location);
         display(weather);
+        await displayImg(weather.conditions);
     } catch (error) {
         alert(`Error fetching weather. ${error.message}`);
     }
@@ -36,5 +38,22 @@ async function getWeather(location) {
 }
 
 function display(weather) {
-    
+    const keys = Object.keys(weather);
+    const headers = ['Condition', 'Temp-Min', 'Temp-Max', 'Temp', 'Humidity'];
+    for (let i = 0; i < 5; i++) {
+        const row = document.createElement('tr');
+        const header = document.createElement('th');
+        const data = document.createElement('td');
+        header.textContent = headers[i];
+        data.textContent = weather[keys[i]];
+        row.appendChild(header);
+        row.appendChild(data);
+        tbody.appendChild(row);
+    }
+}
+
+async function displayImg(condition) {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=QeOY5WAJwWM4BSrlwQ7BelaU09Byth9F&s=${condition}`);
+    const res = await response.json();
+    img.src = res.data.images.original.url;
 }
